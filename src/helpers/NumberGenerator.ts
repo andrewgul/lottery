7fda3148e20e2c51177f2d6ba71924e6b26c96fe
Андрей <andrewgul@yandex.ru>
2021-09-07 20:18:25 +0300

    add number generator

---
+++ src/helpers/NumberGenerator.ts
@@ -0,0 +1,28 @@
+import produce from 'immer'
+
+export default class NumberGenerator {
+    static generateInRange = (min: number, max: number): number => {
+        if (min > max) {
+            throw new Error('min has to be bigger than max')
+        }
+    
+        const random = min + Math.random() * (max + 1 - min)
+    
+        return Math.floor(random)
+    }
+
+    static generateArray = (n: number, min: number, max: number): number[] => {
+        return produce([], (draft: number[]) => {
+            while (draft.length < n) {
+                const generated =
+                    NumberGenerator.generateInRange(min, max)
+        
+                if (draft.includes(generated)) {
+                    continue
+                } else {
+                    draft.push(generated)
+                }
+            }
+        })
+    }
+}
